3617d91d71be5fcf82da07e441959205af810bd4
Hardik Togadiya <hardiktogadiya123@gmail.com>
2021-12-19 23:25:20 +0530

    Update drawer code for mobile view

--- fontend/src/Components/layout/DefaultLayout/defaultDrawer.js
+++ fontend/src/Components/layout/DefaultLayout/defaultDrawer.js
@@ -1,5 +1,15 @@
 import React from "react";
-import {Box, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
+import {
+    Box,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    useMediaQuery,
+    useTheme
+} from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 import {commonTransition} from "../../../HelperFunction";
@@ -10,55 +20,67 @@ const DefaultDrawer = ({routeList, drawerStatus, setDrawerStatus, history}) => {
         setDrawerStatus(drawerStatus => ({...drawerStatus, isNarrow: !drawerStatus.isNarrow}));
     };
 
-    return <Drawer id={"Drawer"} open={drawerStatus.isOpen} anchor={"left"} onClose={() => {
-    }} variant="persistent" sx={{
-        width: (theme) => drawerStatus.isOpen ? drawerStatus.isNarrow ? `calc(${theme.spacing(8.6)})` : `200px` : `0`,
-        ...(commonTransition(`width`)),
-        "& .MuiDrawer-paper": {
-            ...(commonTransition(`transform, width`)),
-            width: (theme) => drawerStatus.isNarrow ? `calc(${theme.spacing(8.6)})` : `200px`,
-            overflowX: `hidden`,
-            position: `relative`,
-        },
-    }}>
-        <Box>
-            <List disablePadding>
-                {routeList.map(eachListItem => <ListItem key={eachListItem.key}
-                                                         button={eachListItem.isButton}
-                                                         selected={history.location.pathname.startsWith(eachListItem.route)}
-                                                         onClick={() => {
-                                                             history.replace(eachListItem.route || '/')
-                                                         }}
-                                                         sx={{
-                                                             px: {xs: 2.75, sm: 2.75},
-                                                             py: {xs: 1.25, sm: 1.25}
-                                                         }}>
-                        <ListItemIcon sx={{minWidth: "46px"}}>{eachListItem.icon}</ListItemIcon>
-                        <ListItemText primary={eachListItem.textValue}/>
-                    </ListItem>
-                )}
-            </List>
-            <List sx={{
-                padding: 0,
-                position: `absolute`,
-                bottom: 0,
-                right: 0,
-                overflow: `hidden`
+    const theme = useTheme();
+    const isGreaterThanSmallBreakpoint = useMediaQuery(
+        theme.breakpoints.up("sm")
+    );
+
+    return (
+        <Drawer
+            id={"Drawer"}
+            variant={isGreaterThanSmallBreakpoint || drawerStatus.isNarrow ? "persistent" : "temporary"}
+            open={drawerStatus.isOpen} anchor={"left"}
+            onClose={() => {
+            }}
+            sx={{
+                width: (theme) => drawerStatus.isOpen ? drawerStatus.isNarrow ? `calc(${theme.spacing(8.6)})` : `200px` : `0`,
+                ...(commonTransition(`width`)),
+                "& .MuiDrawer-paper": {
+                    ...(commonTransition(`transform, width`)),
+                    width: (theme) => drawerStatus.isNarrow ? `calc(${theme.spacing(8.6)})` : `200px`,
+                    overflowX: `hidden`,
+                    position: `relative`,
+                },
             }}>
-                <ListItem sx={{padding: 0}} button={false}>
-                    <IconButton sx={{
-                        margin: `5px 10px`,
-                        transform: `rotate(${drawerStatus.isNarrow ? 540 : 720}deg)`,
-                        transitionProperty: `transform !important`,
-                        transitionDuration: "225ms !important",
-                        transitionTimingFunction: `cubic-bezier(0.25, 0.1, 0.25, 1.0) !important`,
-                        transitionDelay: `0ms !important`,
-                    }} size={`large`} onClick={drawerStatusSetHandler}>
-                        <ArrowBackIosNewIcon/>
-                    </IconButton>
-                </ListItem>
-            </List>
-        </Box>
-    </Drawer>;
+            <Box>
+                <List disablePadding>
+                    {routeList.map(eachListItem => <ListItem key={eachListItem.key}
+                                                             button={eachListItem.isButton}
+                                                             selected={history.location.pathname.startsWith(eachListItem.route)}
+                                                             onClick={() => {
+                                                                 history.replace(eachListItem.route || '/')
+                                                             }}
+                                                             sx={{
+                                                                 px: {xs: 2.75, sm: 2.75},
+                                                                 py: {xs: 1.25, sm: 1.25}
+                                                             }}>
+                            <ListItemIcon sx={{minWidth: "46px"}}>{eachListItem.icon}</ListItemIcon>
+                            <ListItemText primary={eachListItem.textValue}/>
+                        </ListItem>
+                    )}
+                </List>
+                <List sx={{
+                    padding: 0,
+                    position: `absolute`,
+                    bottom: 0,
+                    right: 0,
+                    overflow: `hidden`
+                }}>
+                    <ListItem sx={{padding: 0}} button={false}>
+                        <IconButton sx={{
+                            margin: `5px 10px`,
+                            transform: `rotate(${drawerStatus.isNarrow ? 540 : 720}deg)`,
+                            transitionProperty: `transform !important`,
+                            transitionDuration: "225ms !important",
+                            transitionTimingFunction: `cubic-bezier(0.25, 0.1, 0.25, 1.0) !important`,
+                            transitionDelay: `0ms !important`,
+                        }} size={`large`} onClick={drawerStatusSetHandler}>
+                            <ArrowBackIosNewIcon/>
+                        </IconButton>
+                    </ListItem>
+                </List>
+            </Box>
+        </Drawer>
+    );
 };
 export default DefaultDrawer;
--- fontend/src/config/index.js
+++ fontend/src/config/index.js
@@ -4,9 +4,10 @@ dotenv.config();
 
 
 const config = {
-    BACKEND_BASE_API_URL: process.env.BACKEND_BASE_API_URL || "http://localhost:8080/",
+    // BACKEND_BASE_API_URL: process.env.BACKEND_BASE_API_URL || "http://localhost:8080/",
     // BACKEND_BASE_API_URL: process.env.BACKEND_BASE_API_URL || "http://192.168.43.168:8080/",
     // BACKEND_BASE_API_URL: process.env.BACKEND_BASE_API_URL || "http://192.168.0.184:8080/",
+    BACKEND_BASE_API_URL: process.env.BACKEND_BASE_API_URL || "http://192.168.57.168:8080/",
     APPLICATION_NAME: process.env.APPLICATION_NAME || "BVM",
     POSITION_TYPE_I: process.env.POSITION_TYPE_I || "Admin",
     POSITION_TYPE_II: process.env.POSITION_TYPE_II || "User",
